03a361ccd52a7b25d5c28e5bc148e886cbfe9b2a
Rifat0001 <mahmudrifat764@gmail.com>
2023-05-27 23:19:43 +0600

    get the data from server

--- index.js
+++ index.js
@@ -2,9 +2,15 @@ const express = require('express');
 const app = express();
 const port = process.env.PORT || 5000;
 
+const chefs = require('./data/chefs.json');
+
 app.get('/', (req, res) => {
     res.send('Server is running');
 });
+
+app.get('/chefs', (req, res) => {
+    res.send(chefs)
+})
 
 app.listen(port, () => {
     console.log(`Api is running on port:${port}`)
