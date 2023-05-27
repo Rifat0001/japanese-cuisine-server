ee355fd219691e1945278085bafbd606e27ab94e
Rifat0001 <mahmudrifat764@gmail.com>
2023-05-28 04:39:56 +0600

    data recheck and changed

--- index.js
+++ index.js
@@ -1,8 +1,11 @@
 const express = require('express');
 const app = express();
+const cors = require('cors');
 const port = process.env.PORT || 5000;
 
 const chefs = require('./data/chefs.json');
+
+app.use(cors());
 
 app.get('/', (req, res) => {
     res.send('Server is running');
@@ -10,6 +13,13 @@ app.get('/', (req, res) => {
 
 app.get('/chefs', (req, res) => {
     res.send(chefs)
+})
+
+app.get('/chefs/:id', (req, res) => {
+    const id = req.params.id;
+    const selectedChef = chefs.find(n => n.id == id);
+    console.log(selectedChef)
+    res.send(selectedChef)
 })
 
 app.listen(port, () => {
